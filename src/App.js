514d672c94733591bbd5a3fddda96e11b09ad3f9
Josh-Wickens <joshwickens@hotmail.com>
2023-01-17 19:47:19 +0000

    finishing touches and bug fixes

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import ProfilePage from "./profiles/ProfilePage";
 import UsernameForm from "./profiles/UsernameForm";
 import UserPasswordForm from "./profiles/UserPasswordForm";
 import ProfileEditForm from "./profiles/ProfileEditForm";
+import NotFound from "./components/NotFound";
 
 function App() {
   const currentUser = useCurrentUser();
@@ -73,7 +74,7 @@ function App() {
             render={() => <ProfileEditForm />}
           />
 
-          <Route render={() => <p>Page not found!</p>} />
+          <Route render={() => <NotFound />} />
         </Switch>
       </Container>
     </div>
